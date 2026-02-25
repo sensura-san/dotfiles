751c6db0a9c3d3bbff64dcde4b0b95ae355f165b
sensura-san <gohzhixuan2006@gmail.com>
2026-02-25 09:37:38 +0800

    config: set font in ghostty, add aliases in zsh

--- darwin/home/modules/zsh.nix
+++ darwin/home/modules/zsh.nix
@@ -1,5 +1,18 @@
 {
   programs.zsh = {
     enable = true;
+    shellAliases = {
+      # https://home-manager.dev/manual/25.05/options.xhtml#opt-programs.zsh.shellAliases
+      l = "eza -F";
+      ls = "eza";
+      la = "eza -a"; # same as eza -A
+      ll = "eza -alF";
+
+      ".." = "cd ..";
+
+      v = "nvim";
+      vi = "nvim";
+      vim = "nvim";
+    };
   };
 }
