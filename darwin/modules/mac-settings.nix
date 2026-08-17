319992764d5d7ccee9957aa4e30543c9d5a82b87
sensura-san <gohzhixuan2006@gmail.com>
2026-08-17 21:00:43 +0800

    fix: press and hold now enabled (true was wrong)

--- darwin/modules/mac-settings.nix
+++ darwin/modules/mac-settings.nix
@@ -1,7 +1,7 @@
 # for misc. mac settings
 {
   # enable press-and-hold repeat for keyboard keys, instead of inputting accents
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = true;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
 
   # nix-darwin: enable touch ID for sudo in sudo_local
   # https://nix-darwin.github.io/nix-darwin/manual/#opt-security.pam.services.sudo_local.touchIdAuth
